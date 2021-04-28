ef045eec245be39bc10c87792a486b78587b169f
송재호 <sky4868@hanmail.net>
2021-04-28 13:12:39 +0900

    First Commit

---
+++ js003.js
@@ -0,0 +1,17 @@
+window.onload = function()
+{
+    var canvas = document.getElementById("mycanvas");
+    var ctx = canvas.getContext("2d");
+    strokeRoundedRect(ctx,10,10,100,80,10);
+    strokeRoundedRect(ctx,150,10,100,80,30);
+    function strokeRoundedRect(ctx, x, y, width, height, radius)
+    {
+        ctx.beginPath();
+        ctx.moveTo(x+radius,y);
+        ctx.arcTo(x+width,y, x+width,y+height,radius);
+        ctx.arcTo(x+width,y+height, x, y+height,radius);
+        ctx.arcTo(x, y+height, x,y,radius);
+        ctx.arcTo(x, y, x+width, y, radius);
+        ctx.stroke();
+    }
+}
