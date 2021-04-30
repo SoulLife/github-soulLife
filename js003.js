d65586571682afc4df65dbd16c5736ca3cd35d18
송재호 <sky4868@hanmail.net>
2021-04-30 17:57:30 +0900

    no message

--- js003.js
+++ js003.js
@@ -1,24 +1,10 @@
 window.onload = function()
-{
-    function binarySearch(x, a)
-    {        
-        var left = 0, right = a.length-1;
-        while(left < right)
-        {
-            var middle = Math.floor((left + right)/2);
-            console.log("middle:" + middle);
-            if(x <= a[middle])
-            {
-                right = middle;                
-            }else
-            {
-                left = middle+1;                
-            }            
-        }
-        console.log("left:%d, right:%d:",left,right);
-        if(x == a[right])return right;
-        return null;
+{       
+    function fact(n)
+    {
+        if(n <= 1) return 1;
+        return n*fact(n-1);
     }
-    var a = [2,4,7,12,15,21,34,35,46,57,70,82,86,92,99];    
-    console.log(binarySearch(100,a));
+    console.log(fact(5));
 }
+//
