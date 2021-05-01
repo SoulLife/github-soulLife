0b509dacaecc421f2b1cf317bc58fb5b7c42cb03
SONG_JAE_HO <sky4868@hanmail.net>
2021-05-01 23:50:20 +0900

    no message

--- js003.js
+++ js003.js
@@ -1,10 +1,12 @@
 window.onload = function()
 {       
-    function fact(n)
+    function hanoi(n, a,b,c)
     {
-        if(n <= 1) return 1;
-        return n*fact(n-1);
+        if(n < 1) { return; }
+        hanoi(n-1,a,b,c);
+        console.log("%d 번째 원반: %s -> %s",n,a,c);
+        hanoi(n-1,b,a,c);
     }
-    console.log(fact(5));
+    hanoi(4,"A","B","C");
 }
 //
