c04576dfe85b60647d63c025803e9e587a4a6e24
SONG_JAE_HO <sky4868@hanmail.net>
2021-05-03 21:25:14 +0900

    no message

--- js003.js
+++ js003.js
@@ -1,5 +1,4 @@
 window.onload = function()
-<<<<<<< HEAD
 {   
     var pow = function(exponent)
     {
@@ -12,26 +11,4 @@ window.onload = function()
     var sqrt = pow(5);
     var cubicroot = pow(1/3);
 };
-=======
-{       
-    var Module = Module || {};
-    (function(_Module)
-    {
-         var name = "NoName";
-         function getName()
-         {
-             return name;
-         }   
-         _Module.showName = function()
-         {
-             console.log(getName());
-         };
-         _Module.setName = function()
-         {
-             name = x;
-         }
-    })(Module);
-    Module.setName("Tom");
-    Module.showName();
-}
->>>>>>> 0d805207e6c5b9a35d8c7a71389491ddac0a695b
+
