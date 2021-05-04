07e7b1069459931ab485ab30233f56f332a66750
송재호 <sky4868@hanmail.net>
2021-05-04 17:37:20 +0900

    불변 얕은복사 깊은복사

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,5 +1,35 @@
 window.onload = function()
 {   
-        
+    var user = { 
+        name:"Jaenam",
+        urls:{
+            portfolio: "http://github.com/abc",
+            blog: "http://blog.com",
+            facebook: "http://facebook.com/abc"
+        }
+    };
+    var copyObject = function(target)
+    {
+        var result = {};
+        if(typeof target === 'object' && target !== null)
+        {
+            for(var prop in target)
+            {
+                result[prop] = copyObject(target[prop]);
+            }
+        }else
+        {
+            result = target;
+        }        
+        return result;
+    };
+    var user2 = copyObject(user);
+    user2.name = "Jung";
+    console.log(user.name === user2.name);
+    user.urls.portfolio = "http://portpolio.com";
+    console.log(user.urls.portfolio === user2.urls.portfolio);
+
+    user2.urls.blog = '';
+    console.log(user.urls.blog === user2.urls.blog);
 };
 
