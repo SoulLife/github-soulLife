a298c58afcc2619c31b08979fd7bf3509ad1c51f
SONG_JAE_HO <sky4868@hanmail.net>
2021-05-05 21:29:57 +0900

    실행 컨텍스트 의 VariableEnvironment와 LexicalEnvironment개념 environmentRecord와 호이스팅
    함수 선언문과   함수표현식 개념 공부

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,19 +1,13 @@
-window.onload = function()
-{   
-    var arr1 = [undefined,1];
-    var arr2 = [];
-    arr2[1] = 1;
 
-    arr1.forEach(function(v,i){ console.log(v,i);});
-    arr2.forEach(function(v,i){ console.log(v,i);});
+    console.log(sum(1,2));    
 
-    arr1.map(function(v,i){ return v + i;});
-    arr2.map(function(v,i){ return v + i;});
+    function sum(a, b)
+    {
+        return a + b;
+    }
+    var multiply = function (a, b){
+        return a * b;
+    }    
+    console.log(multiply(3,4));
 
-    arr1.filter(function(v){ return !v;});
-    arr2.filter(function(v){ return !v;});
-
-    arr1.reduce(function(p,c,i){ return p + c+ i;});
-    arr2.reduce(function(p,c,i){ return p + c+ i;});
-};
 
