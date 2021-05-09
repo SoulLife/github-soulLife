a2936dedbc6d7521860dc9b0151865b039a46c63
SONG_JAE_HO <sky4868@hanmail.net>
2021-05-09 20:39:37 +0900

    bind메서드 콜백 함수내에서의 this

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,31 +1,21 @@
 window.onload = function()
 {
-    var str = "abc def";    
-    Array.prototype.concat.call(str,"string");
-    Array.prototype.every.call(str, function(char){ return char !== ' ';});
-    Array.prototype.some.call(str, function(char) { return char === ' ';});
-    var newArr = Array.prototype.map.call(str, function(char) { return char + '!';});
-    //console.log(newArr);
-    var newStr = Array.prototype.reduce.apply(str, [function(string, char, i){ return string + char + i;},'']);
-    //console.log(newStr);
-    
-    function Person(name, gender)
-    {
-        this.name = name;
-        this.gender = gender;
-    }
-    function Student(name, gender, school)
-    {
-        Person.call(this,name,gender);
-        this.school = school;
-    }
-    function Employee(name, gender, company)
-    {
-        Person.apply(this,[name,gender]);
-        this.company = company;
-    }
-    var by = new Student("보영","female","단국대");
-    var jn = new Employee("재난","male","구골");    
+    var report = {
+        sum: 0,
+        count: 0,
+        add: function(){
+            var args = Array.prototype.slice.call(arguments);
+            args.forEach(function (entry){
+                this.sum += entry;
+                ++this.count;                
+            },this);
+        },
+        average:function(){
+            return this.sum / this.count;
+        }
+    };
+    report.add(60,85,95);
+    console.log(report.sum,report.count,report.average());
 }
 
 
