12c79dbb7e52b0a3555b5e3fe2d41c56600a7f90
송재호 <sky4868@hanmail.net>
2021-05-10 17:37:39 +0900

    Revert "콜백함수 설명 및 예제"
    This reverts commit 5e89e3208ed8ccb5b8057e87ca2dc791bbfb1ebf.

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,35 +1,14 @@
 window.onload = function()
 {
-    var count = 0;
-    var cbFunc = function()
+    var func = function(x)
     {
-        //console.log(count);
-        if(++count > 4)clearInterval(timer);        
+        console.log(this,x);
     };
-    var timer = setInterval(cbFunc,300);
-    var newArr = [10,20,30].map(function(currentValue,index){
-        //console.log(currentValue,index);
-        return currentValue + 5;
-    });
-    //console.log(newArr);
-    Array.prototype.map = function(callback, thisArg){
-        var mappedArr = [];
-        for(var i=0;i<this.length;i++)
-        {
-            var mappedValue = callback.call(thisArg || window.this[i], i, this);
-            mappedArr[i] = mappedValue;
-        }
-        return mappedArr;
+    func(1);
+    var obj = {
+        method: func
     };
-    setTimeout(function() { console.log(this);}, 300);
-    [1,2,3,4,5].forEach(function(x)
-    {
-        console.log(this);
-    });
-    document.body.innerHTML += '<button id="a">클릭</button>';
-    document.body.querySelector("#a").addEventListener("click",function(e){
-        console.log(this,e);
-    })
+    obj.method(2);
 }
 
 
