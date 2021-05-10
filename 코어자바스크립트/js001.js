bfaabc3acf0566f3e8a6743b789415e05c5c7258
송재호 <sky4868@hanmail.net>
2021-05-10 17:38:47 +0900

    콜백함수 설명 및 제어권, 인자, this

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,21 +1,35 @@
 window.onload = function()
 {
-    var report = {
-        sum: 0,
-        count: 0,
-        add: function(){
-            var args = Array.prototype.slice.call(arguments);
-            args.forEach(function (entry){
-                this.sum += entry;
-                ++this.count;                
-            },this);
-        },
-        average:function(){
-            return this.sum / this.count;
-        }
+    var count = 0;
+    var cbFunc = function()
+    {
+        //console.log(count);
+        if(++count > 4)clearInterval(timer);        
     };
-    report.add(60,85,95);
-    console.log(report.sum,report.count,report.average());
+    var timer = setInterval(cbFunc,300);
+    var newArr = [10,20,30].map(function(currentValue,index){
+        //console.log(currentValue,index);
+        return currentValue + 5;
+    });
+    //console.log(newArr);
+    Array.prototype.map = function(callback, thisArg){
+        var mappedArr = [];
+        for(var i=0;i<this.length;i++)
+        {
+            var mappedValue = callback.call(thisArg || window.this[i], i, this);
+            mappedArr[i] = mappedValue;
+        }
+        return mappedArr;
+    };
+    setTimeout(function() { console.log(this);}, 300);
+    [1,2,3,4,5].forEach(function(x)
+    {
+        console.log(this);
+    });
+    document.body.innerHTML += '<button id="a">클릭</button>';
+    document.body.querySelector("#a").addEventListener("click",function(e){
+        console.log(this,e);
+    })
 }
 
 
