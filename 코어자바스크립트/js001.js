9f93ad2a2c15d095dc2452c1655a40aae6d2d884
SONG_JAE_HO <sky4868@hanmail.net>
2021-05-10 21:22:53 +0900

    콜백함수 내부의 this에 다른값 바인딩하기

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,35 +1,16 @@
 window.onload = function()
 {
-    var count = 0;
-    var cbFunc = function()
-    {
-        //console.log(count);
-        if(++count > 4)clearInterval(timer);        
-    };
-    var timer = setInterval(cbFunc,300);
-    var newArr = [10,20,30].map(function(currentValue,index){
-        //console.log(currentValue,index);
-        return currentValue + 5;
-    });
-    //console.log(newArr);
-    Array.prototype.map = function(callback, thisArg){
-        var mappedArr = [];
-        for(var i=0;i<this.length;i++)
+    var obj1 = {
+        name:"obj1",
+        func:function()
         {
-            var mappedValue = callback.call(thisArg || window.this[i], i, this);
-            mappedArr[i] = mappedValue;
+            var self = this;
+            return function()
+            {
+                console.log(self).name;
+            };
         }
-        return mappedArr;
     };
-    setTimeout(function() { console.log(this);}, 300);
-    [1,2,3,4,5].forEach(function(x)
-    {
-        console.log(this);
-    });
-    document.body.innerHTML += '<button id="a">클릭</button>';
-    document.body.querySelector("#a").addEventListener("click",function(e){
-        console.log(this,e);
-    })
+    var callback = obj1.func();
+    setTimeout(callback,1000);
 }
-
-
