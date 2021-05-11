583c6b9109314d0347ff341da4c64730eeec5974
송재호 <sky4868@hanmail.net>
2021-05-11 17:59:27 +0900

    연습문제 풀이

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,16 +1,54 @@
 window.onload = function()
 {
-    var obj1 = {
-        name:"obj1",
-        func:function()
-        {
-            var self = this;
-            return function()
+    class ObjectUtilities
+    {
+        static mergeObjects = (objA, objB) => ({...objA, ...objB});
+        static removePassword = (obj) => {
+            const tempObj = {...obj};
+            delete tempObj.password;
+            return tempObj;
+        };
+        static getOnlyVlaues = (obj) => {
+            const arr = [];              
+            for(var tempObject in obj)
             {
-                console.log(self).name;
-            };
+                arr.push(obj[tempObject]);
+            }
+            return arr;
+        };
+        static getOnlyProperties = (obj) =>{
+            const arr = [];
+            for(var tempObject in obj)
+            {
+                arr.push(tempObject);
+            }
+            return arr;
+        }
+        static freezeObj  = (obj)=>{
+           const tempObj = {...obj};
+           Object.freeze(tempObj);
+           return tempObj;
         }
     };
-    var callback = obj1.func();
-    setTimeout(callback,1000);
+    
+    const objA = {
+        name: "Nicolas",
+        favFood: "Kimchi"
+    };
+    const objB =
+    {
+        password: "12345"
+    }
+
+    const user = ObjectUtilities.mergeObjects(objA,objB);
+    console.log(user);
+    const cleanUser = ObjectUtilities.removePassword(user);    
+    console.log(cleanUser);
+    const freezeUser = ObjectUtilities.freezeObj(cleanUser);
+    const arrValueUser = ObjectUtilities.getOnlyVlaues(freezeUser);
+    console.log(arrValueUser);
+    const arrProperties = ObjectUtilities.getOnlyProperties(freezeUser);
+    console.log(arrProperties);
+    
 }
+
