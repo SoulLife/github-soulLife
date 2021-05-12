d9100bb267bea1d4dfbe7b7c7ea82022fc9ea581
송재호 <sky4868@hanmail.net>
2021-05-12 17:54:59 +0900

    Revert "Revert "Revert "비동기지옥 제어 탈출 콜백 메서드 Promiss사용"""
    This reverts commit 69f8d848018d50d07fcd9e0d766cded59fbc421c.

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,36 +1,54 @@
 window.onload = function()
 {
-    new Promise(function(resolve){
-        setTimeout(function(){
-            var name = "에스프레소";
-            console.log(name);
-            resolve(name);
-        },500);        
-    }).then(function(prevName){
-        return new Promise(function(resolve){
-            setTimeout(function(){
-                var name = prevName + ", 아메리카노";
-                console.log(name);
-                resolve(name);
-            },500);
-        });        
-    }).then(function(prevName){
-        return new Promise(function(resolve){
-            setTimeout(function(){
-                var name = prevName + ", 카페모카";
-                console.log(name);
-                resolve(name);
-            },500);
-        });
-    }).then(function(prevName){
-        return new Promise(function(resolve){
-            setTimeout(function(){
-                var name = prevName + ", 카페모카";
-                console.log(name);
-                resolve(name);
-            },500);
-        });
-    });
+    class ObjectUtilities
+    {
+        static mergeObjects = (objA, objB) => ({...objA, ...objB});
+        static removePassword = (obj) => {
+            const tempObj = {...obj};
+            delete tempObj.password;
+            return tempObj;
+        };
+        static getOnlyVlaues = (obj) => {
+            const arr = [];              
+            for(var tempObject in obj)
+            {
+                arr.push(obj[tempObject]);
+            }
+            return arr;
+        };
+        static getOnlyProperties = (obj) =>{
+            const arr = [];
+            for(var tempObject in obj)
+            {
+                arr.push(tempObject);
+            }
+            return arr;
+        }
+        static freezeObj  = (obj)=>{
+           const tempObj = {...obj};
+           Object.freeze(tempObj);
+           return tempObj;
+        }
+    };
+    
+    const objA = {
+        name: "Nicolas",
+        favFood: "Kimchi"
+    };
+    const objB =
+    {
+        password: "12345"
+    }
+
+    const user = ObjectUtilities.mergeObjects(objA,objB);
+    console.log(user);
+    const cleanUser = ObjectUtilities.removePassword(user);    
+    console.log(cleanUser);
+    const freezeUser = ObjectUtilities.freezeObj(cleanUser);
+    const arrValueUser = ObjectUtilities.getOnlyVlaues(freezeUser);
+    console.log(arrValueUser);
+    const arrProperties = ObjectUtilities.getOnlyProperties(freezeUser);
+    console.log(arrProperties);
     
 }
 
