5f4bb8964316ac7414abdd87c4b0d91785174fdd
송재호 <sky4868@hanmail.net>
2021-05-13 15:12:49 +0900

    콜백함수 동기/비동기 사용 (async, await)

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,35 +1,26 @@
 window.onload = function()
 {
-    new Promise(function(resolve){
-        setTimeout(function(){
-            var name = "에스프레소";
-            console.log(name);
-            resolve(name);
-        },500);        
-    }).then(function(prevName){
-        return new Promise(function(resolve){
-            setTimeout(function(){
-                var name = prevName + ", 아메리카노";
-                console.log(name);
-                resolve(name);
-            },500);
-        });        
-    }).then(function(prevName){
-        return new Promise(function(resolve){
-            setTimeout(function(){
-                var name = prevName + ", 카페모카";
-                console.log(name);
-                resolve(name);
-            },500);
-        });
-    }).then(function(prevName){
-        return new Promise(function(resolve){
-            setTimeout(function(){
-                var name = prevName + ", 카페모카";
-                console.log(name);
-                resolve(name);
-            },500);
-        });
-    });
+   var addCoffee = function(name){
+       return new Promise(function(resolve){
+           setTimeout(function(){
+               resolve(name);               
+           },500);
+       });
+   };
+   var coffeeMaker = async function (){
+       var coffeeList = '';
+       var _addCoffee = async function(name){
+           coffeeList += (coffeeList ? ',':'') + await addCoffee(name);
+           await _addCoffee("에스프레소");
+           console.log(coffeeList);
+           await _addCoffee("아메리카노");
+           console.log(coffeeList);
+           await _addCoffee("카페모카");
+           console.log(coffeeList);
+           await _addCoffee("카페라때");
+           console.log(coffeeList);
+       }
+   };
+   coffeeMaker();
 }
 
