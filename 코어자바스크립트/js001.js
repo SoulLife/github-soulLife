0b9e36d254074660aaf7e2f090620247de7c53c3
송재호 <sky4868@hanmail.net>
2021-05-14 17:39:16 +0900

    클로저의 의미및 원리 이해예제

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,10 +1,20 @@
 window.onload = function()
 {
-   var now = new Date();
-   console.log(typeof(now +1));
-   console.log(typeof(now - 1));
-   console.log(now == now.toString());
-   console.log(now > (now-1));
+    window.onload = function()
+    {
+       (function(){
+           var count = 0;
+           var button = document.createElement("button");
+           button.innerText = "click";
+           button.addEventListener("click",function(){
+               console.log(++count,"times clicked");
+           });
+           document.body.appendChild(button);
+       })();
+    
+    }
+    
+    
 
 }
 
