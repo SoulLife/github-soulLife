013dac40caa6ba8d33b5d4bc7422b1c5bc7f7600
송재호 <sky4868@hanmail.net>
2021-05-14 17:38:46 +0900

    Revert "클로저의 의미 및 원리 이해"
    This reverts commit 7000ba8afce3caff956d9942d2c9cd6cac0b3e20.

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,14 +1,26 @@
 window.onload = function()
 {
-   (function(){
-       var count = 0;
-       var button = document.createElement("button");
-       button.innerText = "click";
-       button.addEventListener("click",function(){
-           console.log(++count,"times clicked");
+   var addCoffee = function(name){
+       return new Promise(function(resolve){
+           setTimeout(function(){
+               resolve(name);               
+           },500);
        });
-       document.body.appendChild(button);
-   })();
-
+   };
+   var coffeeMaker = async function (){
+       var coffeeList = '';
+       var _addCoffee = async function(name){
+           coffeeList += (coffeeList ? ',':'') + await addCoffee(name);
+           await _addCoffee("에스프레소");
+           console.log(coffeeList);
+           await _addCoffee("아메리카노");
+           console.log(coffeeList);
+           await _addCoffee("카페모카");
+           console.log(coffeeList);
+           await _addCoffee("카페라때");
+           console.log(coffeeList);
+       }
+   };
+   coffeeMaker();
 }
 
