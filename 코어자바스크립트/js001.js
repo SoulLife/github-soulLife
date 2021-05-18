578213d1999f4d5e0a7339937a5eefbe84d54fc6
송재호 <sky4868@hanmail.net>
2021-05-18 17:28:48 +0900

    콜백함수 클로저의 예시사례 예제

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,17 +1,18 @@
 window.onload = function()
 {
-    var i; //하나의 변수를 선언함
-    var j = 0; //하나의 var와 값을 선언함
-    var p, q; //두변수를 선언함
-    var greeting = "hello" + name;//복잡한 초기 값 표현식
-    var x = 2.34, y = Math.cos(0.75), r, theta; //많은 변수들이 선언됨
-    var x = 2, y = x*x;
-    var x = 2, f = function(x) { return x * x;}, y = f(x); //두번째 변수 y에서  첫번째 변수 x를 사용함
-    //여러변수를 선언할수 있음
-    //각 라인별로 변수를 선언함
-    console.log(x,y,f(2));
-    for(var i=0; i<10; i++)console.log(i);
-    for(var i = 0, j = 10; i< 10; i++,j--)console.log(i * j);
-    for(var i in o)console.log(i);
+    var alertFruit = function(fruit)
+    {
+        alert("your choice is" + fruit);
+    };
+    var fruits = ["apple", "banana", "peach"];
+    var $ul = document.createElement("ul");
+    fruits.forEach(function(fruit){
+        var $li = document.createElement("li");
+        $li.innerText = fruit;
+        $li.addEventListener("click",alertFruit.bind(null,fruit));
+        $ul.appendChild($li);        
+    });
+    document.body.appendChild($ul);
+    alertFruit(fruits[1]);
 }
 
