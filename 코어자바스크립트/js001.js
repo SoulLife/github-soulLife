132bca878f723e4510ea914e9917df57ebedf5cb
SONG_JAE_HO <sky4868@hanmail.net>
2021-05-19 00:03:18 +0900

    switch, while, for, for in,  프로퍼티열거

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,18 +1,32 @@
 window.onload = function()
 {
-    var alertFruit = function(fruit)
+    function convert(x)
     {
-        alert("your choice is" + fruit);
-    };
-    var fruits = ["apple", "banana", "peach"];
-    var $ul = document.createElement("ul");
-    fruits.forEach(function(fruit){
-        var $li = document.createElement("li");
-        $li.innerText = fruit;
-        $li.addEventListener("click",alertFruit.bind(null,fruit));
-        $ul.appendChild($li);        
-    });
-    document.body.appendChild($ul);
-    alertFruit(fruits[1]);
+        switch(typeof x)
+        {
+            case "number": //주어진 숫자를 16진수 정수로 변환한다.
+                return x.toString(16);
+            case "string": //문자열을 큰따옴표로 묶어서 반환한다.
+                return '"' + x + '"';
+            default: //이 외의 타입은 문자열로 변환한다.
+                return String(x);
+        }
+    }
+    var count = 0;
+    while(count < 9)
+    {
+        console.log(++count);
+    }
+    var o = {x:1, y:2, z:3};
+    var a = [], i = 0;
+    for(var p in o) // 비어있음
+    {
+        console.log(o[p]);
+        a[i++] = o[p];
+    }
+    for(i in a)
+    {
+        console.log(a[i]);
+    }
 }
 
