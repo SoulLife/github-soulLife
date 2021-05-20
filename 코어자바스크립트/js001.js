10556017117a91e5adfe2bcd5aefb33661315a8f
SONG_JAE_HO <sky4868@hanmail.net>
2021-05-21 00:03:15 +0900

    객체 리터럴, new를 사용해 객체 생성하기, 프로토타입, Object.create()

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,79 +1,26 @@
 window.onload = function()
 {   
-    var a = [1,2,3,4,5];
-    var target = 3;
-    for(var i=0; i<a.length; i++)
+    var empty = {}; //프로퍼티가 없는 빈 객체
+    var point = {x:0, y:0}; //두 개의 프로퍼티 x,y를 정의한다.
+    var point2 = {x:point.x, y:point.y+1}; //프로퍼티를 수식의 값으로 정의한다.
+    var book = { "main title": "javaScript", "sub-title":"The Definitive Guide", "for": "all audiences"
+    ,author: { firstname: "David", surname:"Flanagan"}}; //프로퍼티 이름은 공백과 하이픈(-)을 포함할 수 있다. 
+                                                        //예약어인 for도 인용부호를 둘러싸서 문자열 리터럴로 사용할 수있다. 프로퍼티 author의 값은 객체 그 자체다. 
+                                                        //이 예에서 볼수 있듯이, 프로퍼티의 이름은 인용부호로 감싸지 않을 수도 있다.
+    var o1 = Object.create({x:1, y:2}); //o1은 x,y 프로퍼티를 상속받는다.
+    //inherit()은 프로토타입 객체 p의 속성을 상속받아 새롭게 생성된 객체를 반환한다. 만일 ECMAScript5 함수인 Object.create()가 정의되어 있다면 
+    //해당 함수를 사용할 수 있다. 하지만 사용할수 없는 경우에는 예전 방법을 사용한다. 
+    function inherit(p)
     {
-        if(a[i] == target) break;
+        if(p == null) throw TypeError(); //p는 null이 아닌 객체여야 한다. 
+        if(Object.create) //만약 Object.create()를 사용할 수 있으면
+            return Object.create(p); //이를 사용한다
+        var t = typeof p;//만일 객체의 타입 검사가 좀 더 필요한 경우 아래와 같이하자
+        if(t !== "object" && t !== "function") throw TypeError();
+        function f(){}; //임시로 빈 생성자 함수를 정의한다. 
+        f.prototype = p; //f의 프로토타입 프로퍼티를 p로 설정한다.
+        return new f(); // p를 상속하는 객체를 만들기 위해서는 함수 f()를 사용한다. 
     }
-    function getData()
-    {
-        var arr = [[1,2,3],[4,5,6]];
-        return arr;
-    }
-    var matrix = getData();
-    //숫자로 채워진 2차원 배열을 가져온다.
-    //배열에 있는 모든 숫자를 합해보자.
-    var sum = 0, success = false;
-    //에러가 발생하면 벗어날 수 있도록 레이블을 붙인 구문으로 시작한다.
-    compute_sum: if(matrix)
-    {
-        for(var x=0; x<matrix.length; x++)
-        {
-            var row = matrix[x];
-            if(!row) break compute_sum;
-            for(var y = 0; y<row.length;y++)
-            {
-                var cell = row[y];
-                if(isNaN(cell))break compute_sum;
-                sum += cell;
-            }
-            
-        }
-        success = true;
-        //break문을 사용하면 이곳으로 건너뛴다. 만약 success == false이면 주어진 배열에 뭔가 문제가 있는 것으로 판단한다. 
-        //그렇지 않은 경우에는 sum변수에 배열에 모든 숫자의 합이 저장되어 있다. 
-    }
-    console.log((success ? "데헤":"이힝"));
-    function display_object(o)
-    {
-        //인자가 null이거나 undefind로 평가되면 즉시 함수를 종료한다.
-        if(!o)return;
-        //함수의 나머지 부분은 여기서 계속된다.
-    }
-    function factorial(x)
-    {
-        //만약 입력 전달인자가 유효하지 않으면 예외를 발생시킨다.!
-        if(x < 0) throw new Error("x must not be negative");
-        //유효하다면 값을 계산하여 정상적으로 반환한다.
-        for(var f = 1; x > 1; f *=x, x--) //비어있음
-        return f;
-    }
-    try
-    {
-        //보통 이 코드는 아무런 문제없이 블록의 시작부터 끝까지 실행된다.
-        // 하지만 경우에 따라 예외가 발생할 수 있다.
-        //예외는 throw문에 의해 직접적으로 발생할 수도 있고,
-        //또는 예외를 발생시키는 메서드의 호출에 의해 발생할 수도 있다.        
-    }catch(e)
-    {
-        //이블록 내부의 구문들은 오직 try블록에서 예외가 발생한 경우에 실행된다.
-        //이 구문들에서는 지역변수 e를 사용해 Error객체 또는
-        //앞에서 던진 다른 값을 참조할 수 있다.
-        // 이 블록에서는 어떻게든 그 예외를 처리할 수도있고,
-        //그냥 아무것도 하지 않고 예외를 무시할 수도 있고.
-        //아니면 throw를 사용해서 예외를 다시 발생시킬 수도 있다.
-    }finally
-    {
-        //이 블록에는 try 블록에서 일어난 일에 관계없이
-        //무조건 실행될 코드가 위치한다. 
-        //이 코드는 try 블록이 어떻게든 종료되면 실행된다.
-        //try블록이 종료되는 상황은 다음과 같다.
-        //1)정상적으로 블록의 끝에 도달했을 때
-        //2)break, continue 또는 return 문에 의해서
-        //3)예외가 발생했지만 앞의 catch절에서 처리되었을 때
-        //4)예외가 발생했고 그것이 잡히지 않은 채 퍼져나갈 때
 
-    }
 }
 
