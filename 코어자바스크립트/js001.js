eb1213ffa39fa1afb425f28d492927ce874292ec
SONG_JAE_HO <sky4868@hanmail.net>
2021-05-23 00:01:19 +0900

    프로퍼티 삭제하기,프로퍼티 검사하기

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,25 +1,11 @@
-window.onload = function () {
-    var empty = {}; //프로퍼티가 없는 빈 객체
-    var point = { x: 0, y: 0 }; //두 개의 프로퍼티 x,y를 정의한다.
-    var point2 = { x: point.x, y: point.y + 1 }; //프로퍼티를 수식의 값으로 정의한다.
+window.onload = function () {    
     var book = {
         "main title": "javaScript",
         "sub-title": "The Definitive Guide",
         for: "all audiences",
         author: { firstname: "David", surname: "Flanagan" },
     }; 
-    var author = book.author; //book 객체에서 author 프로퍼티를 가져온다.
-    var name = author.surname; //author 객체에서 surname 프로퍼티를 가져온다.
-    var title = book["main title"];//book객체에서 'main title' 프로퍼티를 가져온다.
-    book.edition = 6; //book 객체에 edition 프로퍼티를 만든다.
-    book["main title"] = "ECMAScript"; //"main title" 프로퍼티를 만든다.
-
-    var addr = "";
-    for(i =0; i<4; i++)
-    {
-        //addr += customer["address" + i] + "\n";
-    }//customer객체의 address0,address1,address2, address3프로퍼티의 값을 읽고 읽은값을 addr변수에 차례대로 이어붙인다. 
-    // .돗트로 프로퍼티에 접근하는 방법은 프로퍼티의 이름을 반드시 식별자로 표현해야 하는대 배열방식으로 접근하는 방법은 문자열로 표현한다 
+    
 
     function getvalue(portfolio)
     {
@@ -45,34 +31,49 @@ window.onload = function () {
     {
         return Object.create(newObject);
     }
-    var o = {}; //o는 Object.prototype을 상속받은 객체고
-    o.x = 1; //고유 프로퍼티x를 갖는다. 
-    var p = inherit(o); //p는 객체 o와 Object.prototype을 상속받는 객체고,
-    p.y = 2; //고유 프로퍼티 y를 갖는다. 
-    var q = inherit(p); //q는 객체 p와 o, Object.prototype을 상속받는 객체고,
-    q.z = 3; //고유 프로퍼티 z를 갖는다. 
-    var s = q.toString(); //q는 Object.protype을 상속받았기 때문에 toString()을 사용할 수 있다.
-    q.x + q.y; //결과는 3이고, q의 프로퍼티 x와 y는 각각 객체 o와 p에서 상속받았다. 
+    //delete는 상속받은 프로퍼티가 아닌 고유의 프로퍼티만 지울수있다. (상속받은 프로퍼티를 지울려면 해당 프로퍼티가 정의된 프로토타입 객체에서 삭제해야한다.)
+    //삭제에 성공하면 해당 프로토타입 객체를 상속한 모든 객체가 영향을 받고 delete표현식에서 삭제에 성공하거나, 프로퍼티가 존재하지 않아서 아무 영향
+    //도 끼지지 못한 경우에는 항상 true로 평가된다. delete연산자의 피연산자가 올바른 프로퍼티 접근 표현식이 아니라서 아무 효력도 낼수없는 경우에도 true로 평가
+   var o = {x:1}; //o에는 프로퍼티 x가 존재하며, toString 프로퍼티는 상속받았다
+   delete o.x; //객체 o에서 프로퍼티 x를 지우고, true를 반환한다.
+   delete o.x; //x가 존재하지 않기 때문에 아무 일도 하지않고 true를 반환한다.
+   delete o.toString; // toString은 객체 o의 고유 프로퍼티가 아니기 때문에 아무일도 하지않고 true를 반환한다.   
+   delete 1; //말도 안되지만 true를 반환한다.
+   delete Object.prototype;//Object.prototype은 속성 변경이 불가능한 프로퍼티이므로 지울 수 없다.
+   var x = 1; //전역변수 x를 선언한다.
+   delete this.x; //전역객체의 프로퍼티 x는 지울 수 없다. 
+   function f() {} //전역함수 f를 선언한다.
+   delete this.f; // 전역 객체의 함수f역시 지울수 없다. 
 
-    var unitcircle = {r:1}; //객체c는 unitcircle로부터 
-    var c = inherit(unitcircle); //프로퍼티 r을 상속받는다. 
-    c.x = 1; c.y = 1; //객체 c는 고유 프로퍼티, x, y를 갖는다. 
-    c.r = 2; //객체 c는 상속받은 프로퍼티 r을 재정의한다. 
-    unitcircle.r; //=>1: 프로토타입 객체의 프로퍼티 r은 바뀌지 않는다. 
-    console.log("c.r = %d, unitcircle.r=%d",c.r,unitcircle.r);
-
-    //구체적이고 확실한 방법
-    var len = undefined;
-    if(book)
-    {
-        if(book.subtitle)
-        {
-            len = book.subtitle.length;
-        }        
-    }
-    //간단하고 관용적인 방법, subtitle.length값 또는 undefined가 반환된다. 
-    var len = book && book.subtitle && book.subtitle.length;
-    //내장된 생성자의 프로토타입 프로퍼티들은 읽기 전용이다. 
-    Object.prototype = 0; //문제없이 실행되나 Object.prototype은 바뀌지 않고, 예외가 발생하지는 않는다. 
+   var o = {x:1}
+   "x" in o; //객체 o에 고유 프로퍼티 x가 존재하므로 true를 반환한다.
+   'y' in o; //객체 o에 고유 프로퍼티 y가 존재하지 않으므로 false를 반환한다.
+   "toString" in o; //객체 o에 상속받은 프로퍼티 toString가 있기 때문에 true를 반환한다. 
+   o.hasOwnProperty("x"); //객체 o에는 고유 프로퍼티 x가 있으므로 메서드는 true를 반환한다. 
+   o.hasOwnProperty("y"); //객체 o에는 고유 프로퍼티 y가 없으므로 메서드는 false를 반환한다. 
+   o.hasOwnProperty("toString"); //toString은 상속된 프로퍼티이기 때문에 메서드는 false를 반환한다.
+   o = inherit({y:2});
+   o.x = 1;
+   o.propertyIsEnumerable("x"); //열거할수 있는 고유 프로퍼티 x를 가지고 있기 때문에 메서드는 true를 반환한다. 
+   o.propertyIsEnumerable("y"); //y는 상속받은 프로퍼티이기 때문에 메서드는 false를 반환한다. 
+   Object.prototype.propertyIsEnumerable("toString"); //toString은 내장 프로퍼티이고, 열거할수 없기 때문에 false를 반환한다. 
+   //undefined가 아니지만 확인할 때는 in연산자 대신 논리 연산자 !== 를 사용하는편이 훨씬 효과적이다. 
+   o = {x:1};
+   o.x !== undefined; //true객체 o에 프로퍼티 x가 존재한다. 
+   o.y !== undefined; // false 객체 o에 프로퍼티 y가 존재하지 않는다. 
+   o.toString !== undefined; //true: toString는 상속받은 프로퍼티이다. 
+   o = {x:undefined}; //프로퍼티가 분명히 존재하지만 값이 undefined 이다.
+   o.x !== undefined; //false: 프로퍼티가 존재하지만 값이 undefined다.
+   o.y !== undefined; //false: 프로퍼티가 존재하지 않는다. 
+   "x" in o; //true프로퍼티가 존재한다. 
+   "y" in o; //false프로퍼티가 존재하지 않는다
+   delete o.x; //프로퍼티 x를 제거한다. 
+   "x" in o; //false프로퍼티가 더이상 존재하지 않는다. 
+   //객체 o가 가진 프로퍼티 x의 값이 null이나 undefined가 아니라면, 2를 곱한다. 
+   if(o.x != null) o.x *=2;
+   //객체 o가 가진 프로퍼티 x의값이false가 아니라면, 2를 곱한다.
+   //만약 x가 undefined나 null, false, '', 0,NaN중 하나라면 그냥 둔다.
+   if(o.x) o.x *= 2;
+   console.log(o.x);
 
 };
