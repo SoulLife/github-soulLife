d95b370ece8b96a14bb3748a4be1bdebc70cdf72
SONG_JAE_HO <sky4868@hanmail.net>
2021-05-27 00:02:32 +0900

    객체 직렬화하기, 객체 메서드 toString, toLocaleString(), toJSON(), valueOf() 배열의 정의와 배열 만들고 읽고 쓰기

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,51 +1,55 @@
 window.onload = function () {    
-    //객체 리터럴을 통해 만든 객체는 Object.prototype을 객체의 프로토타입으로 설정
-    //new를 사용해 만든 객체는 생성자 함수의 prototype프로퍼티 값이 prototype 속성의 값이 되고 Object.create()메서드로 만든 객체는 메서드의 첫번째
-    //인자가 프로토타입 속성의 값이 된다. 
-    var p = {x:1}; //객체 p를 정의한다
-    var o = Object.create(p); //객체 p를 프로토타입으로 하는 객체를 만든다. 
-    p.isPrototypeOf(o); //true : 객체 o는 객체 p를 상속받는다. 
-    Object.prototype.isPrototypeOf(p); //true : 객체 p는 Object.prototype을 상속받는다. 
+    //객체 직렬화는 객체의 상태를 문자열로 변환하는 과정을 말한다. 이때 생성된 문자열은 나중에 객체 복원에 사용할 수 있다.
+    //ECMAScript 5는 자바스크립트 객체를 직렬화하는 JSON.stringfy()메서드와 직렬화한 문자열을 객체로 복원하는 JSON.parse()메서드를 지원한다. 
+    var o = {x:1, y:{z:[false,null,""]}}; //테스트용 객체를 정의한다. 
+    var s = JSON.stringify(o); //s는 '{'x:1, 'y':{'z':[false,null,;;]}}' 문자열이다.
+    var p = JSON.parse(s); //p는 객체 o를 복사한 객체다.
+    //JSON문법은 자바스크립트 문법의 부분 집합이기 때문에 자바스크립트의 모든 값을 표현할수는 없다. 객체와 배열, 문자열, 유한한수, true, false, null은 모두
+    //직렬화할수있고 반대로 복원할수도있다. Nan(Not a Number)와 Infinity, -Infinity는null로 직렬화된다. Date 객체는 ISO날짜 형식을 따르는 문자열로 직렬화된다.
+    //(Date.toJSON() 함수 참고) 하지만 JSON.parse()함수는 문자열을 Date객체로 복원하지 않는다. Function, RegExp, Error객체와 undefined값은 직렬화하거나 
+    //복원할수없다. JSON.stringfy()메서드는 객체가 가진 열거 가능한 고유 프로퍼티만 직렬화한다. 
 
-    //class속성 객체의 타입에 대한 정보를 담고 있는 문자열이다. ECMAScript3과 ECMAScript5 모두 어떠한 방법으로도 이 속성을 변경할수없고 그 값을
-    //질의하는 것도 아주 간접적으로만 가능하다. 객체의 클래스 정보를 알아보기 위해서는 객체의 toString()메서드를 호출하면된다. 이때 반환되는 스트링의 아홉번째 
-    //문자부터 문자열 끝에서 두번째 문자까지 추출한다. 
-    function classOf(o)
-    {
-        if(o ===null)return "Null";
-        if(o === undefined) return "Undefined";
-        return Object.prototype.toString.call(o).slice(8,-1);
-    }
-    //위의 함수에는 어떠한 자바스크립트 값을 넣어도 동작한다. 숫자,문자열, 불리언 값은 toString()메서드를 호출할때 객체처럼 동작하고 
-    // 추가로 null과 undefined인 경우도 처리하고있다. (ECMAScript5에서는 null과 undefined를 따로 처리할 필요가없다. )
-    classOf(null); //Null
-    classOf(1); // Number
-    classOf(""); //String
-    classOf(false); //Boolean
-    classOf({}); //Object
-    classOf([]); //Array
-    classOf(/./); // Regexp
-    classOf(new Date()); //Date
-    classOf(window); // Window(클라이언트측 호스트 객체)
-    function f() {}; //생성자 함수를 생성한다.
-    classOf(new f()); // Object
+    //toString() 메서드는 어떠한 인자도 받지않고 호출 대상 객체의 값을 어떠한 방식으로든 문자열로 만들어서 반환한다. 자바스크립트는 객체를 문자열로 변환해야 할 때
+    //항상 toString()메서드를 사용한다. 예를 들면 +연산자를 사용해 문자열과 객체를 연결하려 하거나 문자열을 인자로 받는 메서드에 객체를 전달하면 해당 객체의 
+    //toString() 메서드가 호출되어 문자열 변환 작업이 실행된다. 
 
-    //extensible 속성은 객체에 새 프로퍼티를 추가할수 있는지 여부를 결정한다. ECMAScript3에서는 모든 내장 객체와 사용자 정의 객체는 특별한 경우가 아니면 확장할수있고
-    //호스트 객체의 확장상언 구현체에 따라 다르다.
-    //확장할수있는 객체인지 알아보려면 Object.isExtensible() 함수에 해당 객체를 인자로 넘긴다. 객체를 확장할 수 없도록 하려면 
-    //Object.preventExtensions()에 해당 객체를 인자로 넘긴다. Object.preventExtensions()함수를 사용해 객체를 확장할수 없도록 설정하면, 설정하기 전 상태로
-    //돌아갈수 없음에 유의하라.
-    //Object.seal()은 Object.preventExtensions()와 동작이 유사하지만 객체를 확장할수 없게 만들뿐만아니라 객체가 가진 모든 고유 프로퍼티를 설정 불가능 하게
-    //만든다. 다시 말해 객체에 새로운 프로퍼티를 추가할수 없고 기존 프로퍼티의 설정을 바꾸거나 지울수도 없다는 뜻이다. 하지만 writable 속성이
-    //true인 기존 프로퍼티의 값은 변경할수 있다. Object.seal()메서드로 한번 봉인된 객체는 다시 해제할 수 없다. Object.isSealed()메서드를 사용해
-    //객체가 봉인되어 있는지 검사할수 있다. 
-    //Object.freeze()메서드는 객체를 좀더 단단히 잠근다. 객체를 확장할수 없게 만들고 프로퍼티 설정을 바꿀수 없게 바꾼다. 
-    //Object.seal()과 object.freeze()는 주어진 객체의 고유 프로퍼티에만 영향을 미치고, 객체가 가진 프로토타입 객체에는 영향을 미치지 않는다. 객체를 철저히
-    //잠그고싶다면 객체의 프로토타입 체인까지 잠궈야 한다. 
-    //Object.preventExtensions()와 Object.seal(), Object.freeze()메서드들은 인자로 넘겼던 객체를 다시 반환한다. 이점을 이용하면 다음 예시처럼 함수 호출을 한줄로
-    //중첩시킬 수 있다. 
-    //Object.freeze()로 프로토타입을 고정시키고
-    //열거할 수없는 프로퍼티y를 가진 객체를 Object.seal()로 봉인한다. 
-    var o = Object.seal(Object.create(Object.freeze({x:1}),{y: {value:2, writable: true}}));
+    //toString()메서드 외에도 모든 객체는 toLocaleString()메서드를 갖고 있다. 이 메서드의 목적은 객체의 지역화(localized)된 문자열 표현을 변환하는 것이다. 객체의
+    //기본으로 정의된 toLocaleString()메서드는 그 자체로 어떠한 지역화 작업을 하지 않는다. 단순히 toString()을 호출한 결과를 반환한다. 
+
+    //toJSON() 메서드 : Object.prototype에는 toJSON()메서드가 정의되어 있지않다. 하지만 JSON.stringfy()메서드는 직렬화할 객체에 toJSON()메서드가 있는지
+    //찾는다. 만약 직렬화하려는 객체에 toJSON()메서드가 있으면 toJSON()메서드가 호출되고 그결과 값이 원래 객체 대신 직렬화된다. 
+
+    //valueOf()메서드 : toString()메서드와 매우 유사하다. 하지만 이 메서드는 자바스크립트가 객체를 숫자와 같은 다른 원시 타입으로 변환하려 할때 호출된다.
+    //다시말해 객체가 원시 타입값을 필요로하는 문맥 안에서 사용될 때 자바스크립트는 valueOf()메서드를 자동으로 호출한다. 
+    //객체에 기본으로 정의된 valueOf()메서드는 아무런 변환 작업을 하지않는다. 하지만 몇몇 내장 객체들은 자신만의 valueOf()메서드를 정의한다.
+
+    var empty = []; //원소가 없는 배열
+    var primes = [2,3,5,7,11]; //숫자 원소가 5개인 배열
+    var misc = [1.1, true, "a",]; //서로 다른 종류의 세 원소가 존재하며 쉼표로 끝나는 배열
+    //배열 리터럴의 값으로는 상수뿐 아니라 임의의 표현식도 사용할수 있다
+    var base = 1024; 
+    var table = [base,base+1,base+2,base+3];
+    //배열 리터럴은 객체 리터럴 또는 다른 배열 리터럴을 포함할수 있다
+    var b = [[1,{x:1,y:2}],[2,{x:3,y:4}]];
+    //배열 리터럴에서 빠진 부분이 존재할 경우 해당 부분의 원소값은 undefined가 된다
+    var count = [1,,3]; //세 원소중 가운데 원소값은 undefined.
+    var undefs = [,,]; //두 원소 모두 값은 undefined
+    //배열을 만드는 또다른 방법은 Array()생성자를 사용
+    //* 인자없이 호출하는 방법 이방법은 아무 원소도없는 빈배열을 생성하고 생성된 배열은 배열 리터럴[]과 동일하다
+    var a = new Array();
+    //배열의 길이를 의미하는 숫자값을 인자로 주어 호출하는 방법
+    // 이 방법은 생성자의 인자로 전달받은 숫자값만큼 원소를 갖는 배열을 생성 한다. 이러한 방법은 배열에 저장될 원소의 크기를 알고 있고
+    // 미리 공간을 할당할 때 사용한다. 이때 배열에는 어떠한 값도 저장되지 않고 배열의 인덱스 프로퍼티 값("0","1"등등) 역시 존재하지 않는다
+    var a = new Array(10);
+    //두개 이상의 원소 또는 숫자가 아닌 원소값 하나를 명시적으로 지정하는 방법
+    // 이 방법은 생성자의 인자값들이 배열의 원소가 된다. 배열의 생성자를 사용하는 것보다 배열 리터럴을 사용하는편이 대체로 훨씬 간단하다
+    var a = new Array(5,4,3,2,1,"testing, testing");
+    var a = ["world"]; //하나의 원소가 있는 배열로 시작해보자
+    var value = a[0]; //0번째 원소에 값을 읽는다.
+    a[1] = 3.14; //1번째 원소에 값을 쓴다
+    i = 2;
+    a[i] = 3; //2번째 원소값을 쓴다
+    a[i+1] = "hello"; //3번째 원소값을 쓴다
+    a[a[i]] = a[0]; //0번째 원소값과 2번째 원소값을 읽어 3번째 원소에 값을 쓴다 
 
 };
