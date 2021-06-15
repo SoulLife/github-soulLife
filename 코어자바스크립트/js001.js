7b02168da2b407996b86115c2d10a76b43f622f1
DESKTOP-OOR1KPB\sky4868 <sky4868@hanmail.net>
2021-06-16 00:03:07 +0900

    함수로 배열처리하기 이론및 예제,  고차함수 이론및 예제

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,48 +1,122 @@
 window.onload = function () {    
-   
-   //Function() 생성자 : 함수 정의문, 함수 리터럴 표현식 두 경우 모두 function 키워드를 사용하여 함수를 정의한다. 하지만 함수는 Function()생성자를 통해서도 정의될 
-   //수있다.
-   var f = new Function("x","y", "return x*y;");
-   //이 코드는 새 함수를 생성하는데 이 함수는 다음과 같이 이미 익숙한 문법으로 정으ㅟ된 함수와 완전히 같다. 
-   var f = function(x, y) { return x * y;}
-   //Function()생성자는 임의 개수의 문자열 인자를 요구한다. 마지막 인자는 함수 몸체의 텍스트이고 세미콜론으로 구분된 임의의 자바스크립트 구문을 포함할수 있다.
-   //생성자에 전달되는 다른 모든 인자는 함수의 매개변수 이름을 지정하는 문자열이다. 만약 인자가 필요없는 함수를 정의한다면 그냥 함수 몸체에 대한 문자열만 생성자에 전달하면 된다.
-   //Function()생성자는 생성하려는 함수의 이름을 지정하는 어떤 인자도 받지 않는다. 함수 리터럴과 마찬가지로 Function()생성자는 익명 함수를 생성한다. Function()생성자에는 
-   //몇가지 중요한 점이 있다.  1. Function()생성자는 동적으로 자바스크립트 함수를 생성하고 실행 시간에 컴파일되는 것을 가능케 한다. 
-   //2. Function()생성자는 생성자가 호출될 때마다 함수 몸체를 분석(parse)하여 새로운 함수 객체를 생성한다. 루프 내부 또는 자주 호출되는 함수 내에서 생성자를 호출한다면
-   //이는 비효율적이다. 이와 반대로 중첩된 함수나 함수 정의 표현식은 루프 내에 있더라도 매번 재컴파일되지 않는다. 
-   //마지막으로 Function()생성자와 관련하여 매우 중요한 점은 함수 생성자가 생성하는 함수는 어휘적 유효범위(lexical scoping)를 사용하지 않는다는 것이다. 함수 생성자가 
-   //생성한 함수는 언제나 최상위 레벨 함수로 컴파일된다. 다음코드에서 이러한 점을확인할 수 있다. 
-   var scope = "global";
-   function constructFunction()
+   //함수로 배열 처리하기 : 숫자로 이루어진 배열이 있다고 가정하고, 이 배열에 있는 값들의 평균과 표준 편차를 구한다고 하자, 비-함수형 프로그래밍 스타일에서는 다음과 같은 식으로
+   //코드를 작성할 것이다.
+   var data = [1,1,3,5,5]; //숫자 배열
+   //mean은 배열 요소의 합을 배열 요소의 개수로 나눈 값이다.
+   var total = 0;
+   for(var i=0; i<data.length; i++) total += data[i];
+   var mean = total / data.length; //mean값은 3
+
+   //표준 편차를 계산하기 위해 먼저 각 요소의 편차에 대한 제곱을 모두 더한다. 
+   total = 0;
+   for(var i=0; i<data.length; i++)
    {
-      var scope = "local";
-      return new Function("return scope"); //지역 유효 범위를 포착하지 않는다. 
+      var deviation = data[i] - mean;
+      total += deviation * deviation;
    }
-   //이줄은 global을 반환하는데 Function()생성자가 반환한 함수는 지역 유효범위를 사용하지 않기 때문이다. 
-   constructFunction()(); //global
-   //eval()은 자신이 수행되는 유효범위에서 새로운 변수와 함수를 정의한다고 했는데 Function()생성자는 전역 유효범위에서 동작하는 eval()과 같다고 생각하면 될 것이다. 
-   //코드에서 Function()생성자를 사용할 필요는 거의 없을 것이다. 
+   var stddev = Math.sqrt(total/(data.length-1)); //표준 편차는 2이다.
+   //배열 객체의 map()메서드와 reduce()메서드를 사용하면 함수형 스타일로 이와 같은 계산을 간단히 처리할수 있다.
+   //먼저 간단한 두 함수를 정의한다.
+   var sum = function(x,y) { return x + y;};
+   var square = function(x) { return x * x;};
+   //평균과 표준편차를 구하고자 Array의 메서드(map, reduce)에 앞에서 정의한 두 함수를 적용한다. 
+   var data = [1,1,3,5,5];
+   var mean = data.reduce(sum)/data.length;
+   var deviations = data.map(function(x) { return x -mean;});
+   var stddev = Math.sqrt(deviations.map(square).reduce(sum)/(data.length-1));
+   //map()과 reduce()같은 메서드는 ECMAScript5에서 소개된 메서드이고 만약 여러분이 ECMAScript3을 사용하고 있기 때문에 이러한 새로운 메서드들을 사용할수 없다면 어떻게
+   //해야 할까? 이를 위해 다음과같이 별도의 map()과 reduce()를 정의할수 있는데 이미 내장 메서드가 있다면 map()과 reduce()는 별도로 구현한 것을 사용하지 않고 내장 메서드를
+   //사용한다. 
+   //배열 a의 각 요소에 대해 함수 f를 호출하고 results 배열을 반환한다. 만약 Array.prototype.map이 이미 정의되어 있다면 그것을 사용한다. 
+   var map = Array.prototype.map? function(a, f){ return a.map(f);} //map메서드가 존재한다면 그것을 사용한다. 
+   :function(a,f){//존재하지 않으면 별도로 구현한다. 
+      var results = [];
+      for(var i=0, len=a.length; i<len; i++)
+      {
+         if(i in a)results[i] = f.call(null,a[i],i,a);
+      }
+      return results;
+   };
+   //함수 f와 초기값 인자를 사용하여 배열a를 단일 값으로 만든다.(Reduce) 만약 Array.prototype.reduce가 이미 정의되어 있다면 그것을 사용한다. 
+   var reduce = Array.prototype.reduce? function(a,f,initial){ //만약 reduce()메서드가 있다면
+      if(arguments.length > 2)
+         return a.reduce(f,initial); //초기값이 전달되었다면.
+      else return a.reduce(f);//초기값이 없음      
+   }
+   : function(a,f,initial){ //ECMAScript5 명세에 따라 구현
+         var i=0, len = a.length,accumulator;
+         //지정된 초기값이나 a의 첫 번째 값으로 시작한다. 
+         if(arguments.length > 2)accumulator = initial;
+         else{ //배열에서 첫번째 인덱스를 찾는다.
+            if(len ==0)throw TypeError();
+            while(i < len)
+            {
+               if(i in a)
+               {
+                  accumulator = a[i++];
+                  break;
+               }
+               else i++;
+            }
+            if(i == len)throw TypeError();
+         }
+         //배열의 나머지 요소들에 대해 f를 호출한다
+         while(i < len)
+         {
+            if(i in a)
+               accumulator = f.call(undefined,accumulator,a[i],i,a);
+            i++;
+         }
+         return accumulator;
+   };
+   //앞에서 정의한 map()과 reduce()함수를 사용하여 평균과 표준편차를 계산하는 코드를 다음과같이 작성할수 있다.
+   var data = [1,1,3,5,5];
+   var sum = function(x,y){ return x + y;};
+   var square = function(x) { return x * x;};
+   var mean = reduce(data,sum)/data.length;
+   var deviations = map(data, function(x) { return x-mean;});
+   var stddev = Math.sqrt(reduce(map(deviations,square),sum)/(data.length-1));
 
-   //호출가능한 객체 : 진짜 배열은 아니지만 대부분의 경우에배열 처럼 다룰수 있는 유사 배열 객체에 대해 알아보았다. 함수에도 비슷한 개념이 있다. 호출 가능 객체(callable object)는
-   //함수 호출 표현식을 통해 호출할수 있는 객체다. 모든 함수는 호출 가능 객체지만 호출가능 객체가 모든 함수는 아니다. 
-   //오늘날 자바스크립트 구현체에서 함수가 아닌 호출 가능 객체는 두 가지 상황에서 볼 수 있다. 첫 번째는 IE 웹브라우저(버전9이하)는 Window.alert()와 Document.getElementsById()
-   //같은 클라이언트 측 메서드를 구현하는데 원시 Function객체가 아니라 호출 가능한 호스트 객체를 사용했다. 이런 메서드들은 IE에서도 다른 브라우저와 똑같이 동작하지만 실제
-   //Function 객체는 아니다. IE9에서는 진짜 함수를 사용하도록 바뀌었고 따라서 이런 호출 가능 객체는 점차 없어지고 있다. 
-   //호출 가능 객체의 또 다른 일반적인 형태는 RegExp객체다. 많은 브라우저에서 RegExp객체의 exec()메서드를 호출하는 대신 RegExp 객체를 직접적으로 호출할 수 있다. 
-   //이것은 넷스케이프와의 호환성을 확보하기 위해 여러 밴더의 호환 제품이 지원하는 비표준 자바스크립트 기능이다. RegExp를 직접 호출할수 있다고 해서 이를 직접 호출하는 코드를
-   //작성하지 않도록 하라. 이 기능은 더는 사용되지 않을 것이고 미래에는 제거될 것이다. typeof 연산자는 호출가능한 RegExp객체와 상호운용되지 않는다. 몇몇 브라우저는 function을
-   //반환하지만 어떤 브라우저들은 object를 반환한다. 만약 어떤 객체가진짜 함수 객체(그리고 함수 메서드를 가지고 있는지)인지를 알아보고 싶다면 class속성을 테스트해보면 된다.
-   function isFunction(x)
+   //고차함수 : 고차함수(higher-order function)는 하나 이상의 함수를 인자로 받고, 새 함수를 반환하는 함수다. 
+   //이 고차 함수는 자신의 인자를f에 전달하고 f의 반환값에 대해 논리적 부정을 계산하는 함수를 반환한다. 
+   function not(f)
    {
-      return Object.prototype.toString.call(x) === "[object Function]";
+      return function() { //새로운 함수를 반환한다.
+         var result = f.apply(this,arguments); //f 호출
+         return !result; //결과 값을 부정
+      };
    }
-   //이 isFunction()함수가 isArray()함수와 매우 비슷하다는 것에 유의하라. 
-
-   //함수형 프로그래밍 : 자바스크립트는 리스프(Lisp)나 헤스켈(Haskell)같은 함수형 프로그래밍 언어는 아니지만 자바스크립트가 함수를 객체로 취급할 수 있다는 말은
-   //자바스크릅트에서도 함수형 프로그래밍 기법을 사용할수 있다는 의미다. ECMAScript5에서 지원하는 map()과 reduce()같은 배열 메서드는 함수형 프로그래밍 스타일에 적합한 구조를
-   //지니고 있다. 이 절에서는 자바스크립트로 함수형 프로그래밍을 하는데 필요한 기법을 설명한다. 그러나 좋은 프로그래밍 스타일이 어떤 것인지를 주장 하려는 것은 아니며 단지 
-   //자바스크립트 함수가 그 정도로 강력하다는 것만 살펴볼 것이다.
-
+   var even = function(x){ //짝수 여부를 판단하는 함수
+      return x % 2 === 0;
+   };
+   var odd = not(even); //논리적 부정을 수행하는 함수 odd
+   [1,1,3,5,5].every(odd); //true : 모든 배열 요소는 홀수다. 
+   //앞에 나온 not()함수는 고차 함수인대 이는 not()함수가 함수를 인자로 받고 새로 생성한 함수를 반환하기 때문이다. 다른 예로 다음 mapper()함수를 살펴보자 mapper()함수 또한
+   //함수를 인자로 받고 전달받은 함수를 사용하여 하나의 배열을 다른 배열에 매핑하는 함수를 반환한다. 이 함수는 앞에서 정의한 map()함수를 사용하는데 mapper()와 map()두 함수가
+   //어떻게 다른지 이해하는 것이 중요하다.
    
+   //배열을 인자로 받고 ,각 배열 요소에 대해 함수 f를 적용한다. 그리고 이 적용 결과에 대한 배열을 반환하는 함수를 반환한다. 앞에서 다룬map()함수와 비교해보라
+   function mapper(f)
+   {
+      return function(a) { return map(a,f);};
+   }
+   var increment = function(x) { return x + 1;};
+   var incrementer = mapper(increment);
+   incrementer([1,2,3]); //[2,3,4]
+   //여기 좀더 일반적인 예제가 있다 이 예제는 이인자로 두 함수 f와 g를 받아서 f(g())를 계산하는 함수를 반환한다. 
+   //f(g(...))를 계산하는 새로운 함수를 반환한다. 반환되는 함수 h는 모든 전달인자를 g로 전달하고 g의 결과 값을 f에 전달한다. 그리고 f의 결과 값을 반환한다. 
+   //f와 g 모두같은 this 값을 사용하여 호출되는데 이 this 값은 h가 호출될 때의this값과 같은 것이다.
+   function compose(f,g)
+   {
+      return function()
+      {
+         //f는 하나의 값만 넘기기 때문에 call을 사용하고 g에는 값 배열을 넘겨야 하기 때문에 apply를 사용한다. 
+         return f.call(this,g.apply(this,arguments));
+      };
+   }
+   var square = function(x) { return x * x;};
+   var sum = function(x,y) { return x + y;};
+   var squareofsum = compose(square,sum);
+   squareofsum(2,3);
+   //다음 항목에서 다룰 partial()함수와 mmemoize()함수는 좀더 중요한 고차함수들이다. 
 }
