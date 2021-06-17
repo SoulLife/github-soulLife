fc5dc31bb6e97577aa5686b232514b91c6eaf5ea
DESKTOP-OOR1KPB\sky4868 <sky4868@hanmail.net>
2021-06-18 00:01:01 +0900

    메모이제이션 이론 설명및 예제

--- 코어자바스크립트/js001.js
+++ 코어자바스크립트/js001.js
@@ -1,70 +1,34 @@
 window.onload = function () {    
-   //함수의 파셜 애플리케이션 : f의bind()메서드는 지정된 컨텍스트와 지정된 인자 집합을 사용하여 함수 f를 호출하는 함수를 반환한다. 즉 함수를 어떤 객체에 바인딩하고,
-   //부분적으로 인자를 적용한다는 뜻이다. bind()메서드는 대상 함수를 호출하기 위해 부분적으로 인자를 적용할 때 부분 인자를 왼쪽에 적용하는데 이는 bind()에 넘긴 
-   //인자들은 대상 함수에 전달되는 인자 목록의 시작점에 위치한 다는 뜻이다. 하지만 이러한 부분 인자들을 오른쪽에 적용할 수도 있다.
+   //메모이제이션 : 함수형 프로그래밍에서는 이러한 캐싱 방식을 메모이제이션(memoization)이라고 부른다. 다음 코드는 고차함수 memoize()이며 이 함수는 인자로 함수를 받아들이고
+   //그 함수에 메모이제이션을 적용한 새로운 함수를 반환한다. 
+   //함수 f의 결과를 저장한 버전을 반환한다. 함수 f의 모든 인자가 서로 구분할수 있는 문자열 표현일 때만 작동한다. 
+   function memoize(f)
+   {
+      var cache = {}; //결과값을 캐시하는 클로저 상의 객체
 
-   //유사 배열 객체(또는 객체의 뒤쪽 일부분만)를 실제 배열로 변환하는 유틸리티 함수 이후 코드에서 인자 객체를 실제 배열로 변환하는데 사용함 
-   function array(a, n){ return Array.prototype.slice.call(a, n|| 0);}
-   //이 함수에 전달된 인자는 대상 함수의 인자 목록에 대해 왼쪽으로 전달된다. 
-   function partialLeft(f /*...*/){
-      var args = arguments; //외부 인자 배열을 저장한다. 
-      return function() { //함수를 반환
-         var a = array(args, 1); //외부 인자의 1번 인덱스부터 시작(0번은 함수f)
-         a = a.concat(array(arguments)); // 이후 이 내부 함수로 전달된 모든 인자를 추가
-         return f.apply(this, a); //그다음 지금까지 만든 인자 목록으로 f를 호출
-      };
-   }
-   //이 함수에 전달된 인자는 대상 함수의 인자 목록에 대해 오른쪽으로 전달된다. 
-   function partialRight(f /*...*/){
-      var args = arguments; //외부 인자 배열을 저장한다
-      return function(){ //함수를 반환
-         var a = array(arguments); //먼저 내부 함수에 전달된 인자로 시작
-         a = a.concat(array(args,1)); //외부 인자의 1번 인덱스부터 나머지를 추가
-         return f.apply(this, a); //이후 지금까지 만든 인자 목록으로 f를 호출
-      };
-   }
-   //이 함수에 전달된 인자는 템플릿으로 사용된다. 인자 목록에서 정의되지 않은(undefined)값은 내부함수에 전달된 인자의 값으로 채워진다. 
-   function partial(f /*...*/){
-      var args = arguments; 
       return function(){
-         var a = array(args, 1); //외부 인자의 1번 인덱스로부터 시작한다
-         var i=0, j=0; 
-         //args에 대해 루프를 돌며 undefined값을 만나면 내부 함수에 전달된 인자 값으로 설정한다. 
-         for(; i<a.length; i++)
-            if(a[i] === undefined)a[i] = arguments[j++];
-         //이제 남은 내부 인자 값들을 추가한다
-         a = a.concat(array(arguments,j));
-         return f.apply(this, a);
+         //캐시키로 사용하기 위해 인자들을 조합하여 하나의 문자열로 만든다. 
+         var key = arguments.length + Array.prototype.join.call(arguments, ",");
+         if(key in cache)return cache[key];
+         else return cache[key] = f.apply(this,arguments);
       };
    }
-   //세 인자를 전달하는 함수
-   var f = function(x,y,z) { return x*(y - z);};
-   //이 세 파셜 애플리케이션이 서로 어떻게 다른지 살펴보라
-   partialLeft(f, 2)(3,4); //-2 : cjtqjsWo dlswkfh qkdlseldgka : 2 * (3-4)
-   partialRight(f, 2)(3,4); // 6 : 마지막 인자로 바인딩함: 3 * (4-2)
-   partial(f, undefined, 2)(3,4); // -6 : 가운데 인자로 바인딩함: 3 * (2-4)
-
-   //이 파셜 애플리케이션 함수를 사용하면 앞서 정의한 함수들을 활용하는 흥미로운 함수를 쉽게 정의할수있다. 다음 에 몇가지 예제가 있다. 
-   var increment = partialLeft(sum, 1);
-   var cuberoot = partialRight(Math.pow, 1/3);
-   String.prototype.first = partial(String.prototype.charAt,0);
-   String.prototype.last = partial(String.prototype.substr,-1,1);
-   //파셜 애플리케이션은 고차 함수와 결합해서 사용할 때 좀더 흥미롭다. 다음 예제는 앞에서 다룬 not()함수를 조합(composition)과 파셜 애플리케이션을 사용하여 정의하는 방법을 다룸
-   var not = partialLeft(compose,function(x) { return !x;});
-   var even = function(x) { return x % 2 ===0;};
-   var odd = not(even);
-   var isNumber = not(isNaN);
-   //또한 평균과 표준편차를 구하는 방법을 조합과 파셜 애플리케이션을 사용하여 진정한 함수형 프로그래밍 스타일로 구현할수 있다. 
-
-   var data = [1,1,3,5,5]; //외부 데이터
-   var sum = function(x, y) { return x + y;}; //두 기본 함수
-   var product = function(x, y) { return x * y;};
-   var neg = partial(product, -1); //몇가지 다른 것들을 정의
-   var square = partial(Math.pow,undefined, 2);
-   var sqrt = partial(Math.pow,undefined, .5);
-   var reciprocal = partial(Math.pow, undefined, -1);
-
-   //이제 평균과 표준 편차를계산함 모든 함수 호출뿐이며 어떠한 연산자도 사용되지 않았다. 
-   var mean = product(reduce(data, sum), reciprocal(data.length));
-   var stddev = sqrt(product(reduce(map(data, compose(square, partial(sum, neg(mean)))), sum),reciprocal(sum(data.length,-1))));
+   //memoize()함수는 캐시 값을 저장하는데 사용할 새 객체를 생성하고, memoize()의 지역변수에 이 객체를 할당한다. 따라서 이 객체는 반환되는 함수의 내부(클로저)에만 존재한다. 
+   //반환된 함수는 인자들을 문자열로 변환하고 그 문자열을 캐시 객체의 프로퍼티 이름으로 사용한다. 만약 캐시에 값이 있다면 그 값을 바로 반환한다. 만약 캐시된 값이 없으면
+   //지정된 함수를 호출하고 결과 값을 캐시한 다음 해당 함수를 반환한다. 어떻게 memoize()를 사용하는지 보라 
+   //유클리디안 알고리즘(http://en.wikipedia.org/wiki/Euclidean_algorithm)을 사용하여 두 정수의 최대 공약수를 반환한다. 
+   function gcd(a,b) //a와 b에 대한 형식 검사는 생략함
+   {
+      var t; //값을 바꾸기 위한 임시 변수
+      if( a < b)t = b,b = a, a = t; // a >= b임을 보장함
+      while(b != 0)t = b, b = a%b, a = t; //최대 공약수에 대한 유클리드 알고리즘
+      return a;
+   }
+   var gcdmemo = memoize(gcd);
+   gcdmemo(85,187); //17
+   //계산 결과를 저장하는 재귀함수를 작성한다면 일반적으로 원본 함수를 재귀호출하기 보다는 결과가 저장된 버전을 재귀 호출하기를 바랄 것이다. 
+   var factorial = memoize(function(n){
+      return (n <=1)? 1 : n * (factorial(n-1));
+   });
+   factorial(5); //반환 값은 120또한 4,3,2,1에 대한 결과도 캐시된다. 
 }
