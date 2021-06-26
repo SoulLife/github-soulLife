f84412a4c1c320a7ffd7ba5e8835a181c3052cfc
DESKTOP-OOR1KPB\sky4868 <sky4868@hanmail.net>
2021-06-26 19:39:57 +0900

    KeyDown이벤트이론 및 실습 실용예제

---
+++ wpf_c#_work/SayHello/ex1/GrowAndShrink.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+namespace SayHello.ex1
+{
+    //KeyDown 이벤트와 관련된 OnKeyDown 메소드는 키 누름을 통지한다. 키보드의 키를 누르고 뗄때 OnkeyDown과 OnkeyUp메소드가 호출된다. 이 메소드를 오버라이딩해 키 입력을 처리할 수
+    //있다. KeyEventArgs객체의 Key 프로퍼티는Key열거형의 멤버로 설정돼 있으며 누르거나 뗀 키가 어떤 키인지를 알려준다. Left, Top, Width, Height프로퍼티는 모두 부동 소수점 값 이
+    //므로 창의 크기를 늘리거나 줄이기 위해 이 값들을 사용한다. 윈도우 전체 화면보다 더 크거나 작게 해서는 안 되므로 크기가 특정한 값에 도달하면 프로퍼티 값을 더 이상 변경 하지
+    //않고 그대로 유지해야 한다. OnKeyDown과 OnKeyUp메소드는 커서 이동 키나 기능 키에 대한 조작을 처리하는데 유용하게 사용된다. 키보드에서 실제 유니코드 문자를 얻을 때에는 OnTe
+    //xtInput 메소드를 오버라이딩해야 하며 TextCompositionEventArgs인자의 Text 프로퍼티에는 유니코드의 문자열이 저장된다. 일반적으로 이 문자열은 하나의 문자가 되겠지만 음성이나
+    //필기 형태의 입력도 OnTextInput메소드를 호출하게 되는데 이때의 문자열은 더 길어질 것이다. 
+    public class GrowAndShrink : Window
+    {
+        public GrowAndShrink()
+        {
+            Title = "Grow & Shrink";
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            Width = 192;
+            Height = 192;
+        }
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            if (e.Key == Key.Up)
+            {
+                Left -= 0.05 * Width;
+                Top -= 0.05 * Height;
+                Width *= 1.1;
+                Height *= 1.1;
+            }
+            else if (e.Key == Key.Down)
+            {
+                Left += 0.05 * (Width /= 1.1);
+                Top += 0.05 * (Height /= 1.1);
+            }
+        }
+    }
+}
---
+++ wpf_c#_work/SayHello/ex1/TypeYourTitle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+namespace SayHello.ex1
+{
+    //Title 프로퍼티를 따로 지정하지 않고 사용자가 직접 타이핑할수 있는 프로그램이다. 
+
+    //Title이 한 글자 이상인 경우에 한해 백스페이스('\b')만을 따로 처리하고 있다. 그 외의 경우에는 단순히 키보드에서 입력된 글자를 Title 프로퍼티 뒤에 덧붙이고 있다. Window 클래
+    //스에는 창의 외형이나 동작에 영향을 주는 다른 프로퍼티들도 정의돼 있다. WindowStyle 프로퍼티에 WindowStyle 열거형의 멤버를 대입할수 있는데 기본값은 WindowStyle.SingleBorder
+    //Window다. WindowStyle.SingleBorderWindow는 약간 더 미려하지만 클라이언트 영역의 크기는 약간 줄어든다. 대화상자에는 보통 WindowStyle.ToolWindow를 사용한다. 제목 표시줄이 약
+    //간 더 짧고 닫기 버튼은 있지만 최소화 버튼과 최대화 버튼은 없다. 그러나 Alt +Space Bar를 누르면 시스템 메뉴가 생기므로 최소화나 최대화 동작을 수행 할수는 있다. WindowStyle.
+    //None으로 설정하면 창 크기 조절 경계는 표시되지만 제목표시줄이 없어진다. 역시 Alt+Space Bar를 눌러 시스템 메뉴를 부를 수는 있다. Title 프로퍼티가 창에는 나타나지 않지만 작업
+    //표시줄에서는 표시된다. ResizeMode프로퍼티를 통해 창 크기 조절 경계를 나타나게 하거나 사라지게 할 수 있다. 이 프로퍼티에는 ResizeMode 열거형의 멤버를 대입한다. 기본값은 
+    //ResizeMode.CanResizeWithGrip으로 하면 클라이언트 영역의 우측 하단에 창 크기를 조절할수 있는 표시가 생긴다. ResizeMode.CanMinimize로 지정하면 창 크기를 조절하는 경계가 보이지
+    //않으며 최대화 버튼도 사용할수 없고 최소화만 가능하다. 따라서 고정된 크기를 갖는 창의 경우에 유용하게 사용할수 있다. 마지막으로 ResizeMode.NoResize는 최소화, 최대화 버튼도 사
+    //용할수 없으며 크기 조절 경계도 사라진다. 
+
+    //WindowState프로퍼티에 WindowState열거형의 멤버를 대입해서 창이 최초에 표시되는 방식을 결정할 수 있다. 대입할수 있는 값으로는 WindowState.Normal, WindowState.Minimized,WindowS
+    //tate.Maxmized가 있다. 
+
+    //Topmost 프로퍼티를 true로 설정하면 다른 모든 창보다 앞에 나타난다. (이 기능은 신중하게 사용해야 한다. 오직 하나의 창만 맨앞으로 나올수 있으므로 사용자가 기능을 켜고 끌수 있게
+    //하는 것이 좋다) Window클래스에서 다른 중요한 프로퍼티는 Background이다. Control에서 상속받은 프로퍼티로서 클라이언트 영역의 색을 지정한다. 실제 Background프로퍼티로 작업을 하
+    //기에는 아직 색상이란 막연한 용어이다. Background프로퍼티는 Brush타입의 객체이며 배경색을 지정하는데 사용할수 있는 브러시 타입에는 그라디언트 브러시, 비트맵(Bitmap)등의 이미지
+    //를 이용한 브러시 등이 있다. 브러시는 WPF에서 매우 중요한 역할을 하기 때문에 여기서는 두장이나 할애했다. 
+    class TypeYourTitle : Window
+    {
+        protected override void OnTextInput(TextCompositionEventArgs e)
+        {
+            base.OnTextInput(e);
+            if (e.Text == "\b" && Title.Length > 0)
+                Title = Title.Substring(0, Title.Length - 1);
+            else if (e.Text.Length > 0 && !Char.IsControl(e.Text[0]))
+                Title += e.Text;
+
+        }
+    }
+}
